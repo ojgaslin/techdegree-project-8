0209a0c845da0b1cac49e3a7ca2293116bb7feaa
ogaslin <olivia.gaslin@gmail.com>
2020-12-10 22:54:30 -0500

    code comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # techdegree-project-8
 This is an application for the local library. It's purpose is to aid in management of their book collection. Before, the librarian had to enter data manually into the SQLite database. This application provides a more intuitive way to manage the data.
+1) included search bar for book listing page
+2) included pagination for book listing page
--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,10 @@
+//*require express to write handlers for requests w/ different http verbs at different url paths
 const express = require('express');
+//*group route handlers together and access them using common route-prefix express.router
 const router = express.Router();
+//*import book.js under models 
 var Book = require("../models").Book;
-
+//*books array is listed in descending order by author
 const books = [
   {order: [["author", "DESC"]]}
 ]
@@ -9,7 +12,7 @@ const books = [
 /*shows full list of books*/
 router.get('/', function(req, res, next) {
    Book.findAll({order: [["author", "DESC"]]}).then(function(books){
-     //check to see if you have anything for a search parameter  -- see line 51 to see how to access parameter
+     //if search parameter, change to lower case, and find if book includes values that match search parameter
       if(req.query.search){
         var searchTerm = req.query.search.toLowerCase();
         var books = books.filter(book =>
@@ -21,33 +24,36 @@ router.get('/', function(req, res, next) {
       }
       
       if(req.query.pageNumber) {
+        //*parse into integer to do math
         var page = parseInt(req.query.pageNumber);
         var limit = parseInt(req.query.limit);
-
+        //*calculate variables needed for pagination
         var startingIndex = (page - 1) * limit;
         var endingIndex = page * limit;
         var numberOfPages = Math.ceil(books.length/limit);
-
+        //*being sent to index.pug for use
         var results = {}
         results.limit = limit;
+        results.pageNumber = req.query.pageNumber;
+        //*checks if next button is needed
         if(endingIndex < books.length) {
         results.next = {
           pageNumber: page + 1
         }
       }
-
+        //*checks if previous button is needed
         if(startingIndex > 0) {
           results.previous = {
           pageNumber: page - 1
         } 
       }
-
+        //taking a slice from the array
         books = books.slice(startingIndex, endingIndex);
         console.log(books)
         console.log("limit: " + limit)
         console.log("page: " + page)
         console.log("ending index: " + endingIndex)
-
+        //sending variables and their properties for use in index.pug
         res.render("books/index", { pageNumber: page, numberOfPages: numberOfPages, books: books, results: results, title: "All Books"});
       } else{
         res.render("books/index", { books: books, title: "All Books"});
@@ -78,11 +84,14 @@ router.post('/',function(req, res, next) {
 });
 /* GET individual article. */
 router.get("/:id", function(req, res, next){
-  Book.findByPk(req.params.id).then(function(book){//same as line 77.  finds article with id that was in param
-    if(book) {//if it exists
-      res.render("books/show", {book: book, title: book.title});//take the article and return show.pug while passing in the found article data to the pug page.
+    //*finds book with id that was in param
+  Book.findByPk(req.params.id).then(function(book){
+    if(book) {//if book exists
+      //*takes book and returns show.pug while passing in the found book data to pug page
+      res.render("books/show", {book: book, title: book.title});
     } else {
-      res.send(404);//if it didn't exist return 404 not found.
+      //*if doesn't exist return 404 error not found
+      res.send(404);
     }
   }).catch(function(error){
       res.send(500, error);
@@ -92,8 +101,10 @@ router.get("/:id", function(req, res, next){
 
 /* Edit Book List. */
 router.get("/:id/edit", function(req, res, next){
+    //*finds id of book that was in param
    Book.findByPk(req.params.id).then(function(book){
      if(book) {
+       //*if book exists, go to edit book page
        res.render("books/edit", {book: book, title: "Edit Books"});      
      } else {
        res.send(404);
@@ -106,16 +117,19 @@ router.get("/:id/edit", function(req, res, next){
 /*updates book info in the database*/
 router.put('/:id', (req, res, next) => {
   console.log(req.params.id)
+   //*finds id of book that was in param
    Book.findByPk(req.params.id).then(function(book){
       if(book) {
-        console.log(book)
+        //*if book exists, return updated book
         return book.update(req.body);
       } else {
         res.send(404);
       }
     }).then(function(book){
+      //go back to book list
       res.redirect("/books");        
     }).catch(function(error){
+        //render edit form with errors
         if(error.name === "SequelizeValidationError") {
           var book = Book.build(req.body);
           book.id = req.params.id;
@@ -128,8 +142,9 @@ router.put('/:id', (req, res, next) => {
      });
   });
 
-  /*Deletes Book Form, returns to All Books page*/
+  /*prompts if you are sure you want to delete the book*/
   router.get("/:id/delete", function(req, res, next){
+    //*finds id of book that was in param
     Book.findByPk(req.params.id).then(function(book){
       if(book) {
         res.render("books/delete", {book: book, title: "Delete Book"});
@@ -143,6 +158,7 @@ router.put('/:id', (req, res, next) => {
 
 /*deletes a book*/
 router.delete('/:id', (req, res, next) => {
+   //*finds id of book that was in param
    Book.findByPk(req.params.id).then(function(book){  
       if(book) {
         return book.destroy();
